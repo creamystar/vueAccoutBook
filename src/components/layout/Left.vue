35f3f88a960a927f65bd30c976eaa7eec91a4f3c
creamystar <creamystar@naver.com>
2022-01-12 01:08:09 +0900

    새 컨텐츠 뷰 추가

---
+++ src/components/layout/Left.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="left-wrap">
+        <router-link to="/game/memoGrid"><div>memo grid</div></router-link>
+        <router-link to="/game/typingGame"><div>typing game</div></router-link>
+    </div>
+</template>
+<script>
+export default {
+    name: "left",
+}
+</script>
+<style>
+#left-wrap{
+    border-right: 1px solid rgba(50,150,140,0.5);;
+    width: 250px;
+    height: 100%;
+}
+#left-wrap div:hover{
+    background-color: rgba(50,150,140,0.2);
+}
+#left-wrap div{
+    cursor:pointer;
+    text-align: center;
+    opacity: 1;
+    padding-top:10px;
+    padding-bottom:10px;
+    color: #444!important;
+    text-decoration-line: none!important;
+}
+</style>
---
+++ src/views/TypingGame.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="typing-wrap">
+        typing
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+#typing-wrap{
+    height: 100%;
+}
+</style>
