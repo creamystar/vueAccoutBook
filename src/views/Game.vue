bf58ece573856ad29fdf7e8f17cd5425942b89bf
creamystar <creamystar@naver.com>
2022-01-19 00:01:12 +0900

    뷰 프론트

---
+++ src/views/Game.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="game-wrap">
+        왼쪽에서 원하는 게임을 선택하세요.
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+#game-wrap{
+    padding: 20px;
+}
+</style>
